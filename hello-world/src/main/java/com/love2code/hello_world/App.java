5b1ef566bc0daa5ad7abafd06b781fb46a8ae51a
czp <czp_182000@hotmail.com>
2019-07-19 11:31:02 +0200

    Create the starter

---
+++ hello-world/src/main/java/com/love2code/hello_world/App.java
@@ -0,0 +1,13 @@
+package com.love2code.hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
